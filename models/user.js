6871fe5b6166f9a610cb206289b33d5ed2433349
Tyler May <tylermay@Tylers-MacBook-Pro.local>
2017-10-19 12:04:02 -0500

    first commit

---
+++ models/user.js
@@ -0,0 +1,20 @@
+"use strict"
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define(
+    "User",
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      over18: DataTypes.BOOLEAN
+    },
+    {
+      classMethods: {
+        associate: function(models) {
+          // associations can be defined here
+        }
+      }
+    }
+  )
+  return User
+}
